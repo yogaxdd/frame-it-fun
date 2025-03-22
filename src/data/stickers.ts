6da53c4cb3d86ab16db3e4bbd505642ab6a72af5
Yoga Rizki Pratama <110215272+yogaxdd@users.noreply.github.com>
2025-03-22 21:05:56 +0700

    Update stickers.ts

--- src/data/stickers.ts
+++ src/data/stickers.ts
@@ -3,36 +3,36 @@ export const stickers = [
   {
     id: 'sticker1',
     name: 'Heart with Bow',
-    image: '../stickers/ribbon_1f380.png'
+    image: '/stickers/ribbon_1f380.png'
   },
   {
     id: 'sticker2',
     name: 'Sparkling Heart',
-    image: '../stickers/sparkling-heart_1f496.png'
+    image: '/stickers/sparkling-heart_1f496.png'
   },
   {
     id: 'sticker3',
     name: 'Whale',
-    image: '../stickers/spouting-whale_1f433.png'
+    image: '/stickers/spouting-whale_1f433.png'
   },
   {
     id: 'sticker4',
     name: 'Pink Bow',
-    image: '../stickers/heart-with-ribbon_1f49d.png'
+    image: '/stickers/heart-with-ribbon_1f49d.png'
   },
   {
     id: 'sticker5',
     name: 'Pink Heart',
-    image: '../stickers/pink-heart_1fa77.png'
+    image: '/stickers/pink-heart_1fa77.png'
   },
   {
     id: 'sticker6',
     name: 'Star',
-    image: '../stickers/star_2b50.png'
+    image: '/stickers/star_2b50.png'
   },
   {
     id: 'sticker7',
     name: 'Lion',
-    image: '../stickers/lion_1f981.png'
+    image: '/stickers/lion_1f981.png'
   }
 ];
