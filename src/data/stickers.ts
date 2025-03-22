ff0972857fe80bb4188d55f14f16e12ffec8b820
Yoga Rizki Pratama <110215272+yogaxdd@users.noreply.github.com>
2025-03-22 21:04:09 +0700

    Update stickers.ts

--- src/data/stickers.ts
+++ src/data/stickers.ts
@@ -3,36 +3,36 @@ export const stickers = [
   {
     id: 'sticker1',
     name: 'Heart with Bow',
-    image: '/lovable-uploads/26317d9e-1ebd-423f-8e80-11e536233e8e.png'
+    image: '../stickers/ribbon_1f380.png'
   },
   {
     id: 'sticker2',
     name: 'Sparkling Heart',
-    image: '/lovable-uploads/b5b5dcbc-5b9f-4f9e-b936-fdc3091e439c.png'
+    image: '../stickers/sparkling-heart_1f496.png'
   },
   {
     id: 'sticker3',
     name: 'Whale',
-    image: '/lovable-uploads/69872cfa-d876-47e1-b8a8-6a96e7616376.png'
+    image: '../stickers/spouting-whale_1f433.png'
   },
   {
     id: 'sticker4',
     name: 'Pink Bow',
-    image: '/lovable-uploads/170886a3-6668-416f-8da0-fb8efdee1d44.png'
+    image: '../stickers/heart-with-ribbon_1f49d.png'
   },
   {
     id: 'sticker5',
     name: 'Pink Heart',
-    image: '/lovable-uploads/63f0d307-cade-4319-9df6-66410597b68c.png'
+    image: '../stickers/pink-heart_1fa77.png'
   },
   {
     id: 'sticker6',
     name: 'Star',
-    image: '/lovable-uploads/8d94c525-8bc1-477e-a595-436f947676f7.png'
+    image: '../stickers/star_2b50.png'
   },
   {
     id: 'sticker7',
     name: 'Lion',
-    image: '/lovable-uploads/831c5b62-7deb-4f46-a38f-981e79d40c17.png'
+    image: '../stickers/lion_1f981.png'
   }
 ];
